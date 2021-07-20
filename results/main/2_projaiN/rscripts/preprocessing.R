617cb664c14623c0f7d7e82582e21fc90f3cdf6e
judith-tonhauser <tonhauser.1@osu.edu>
2021-07-20 10:19:58 +0200

    analyzed negation and modal main experiments

---
+++ results/main/2_projaiN/rscripts/preprocessing.R
@@ -0,0 +1,193 @@
+# negation: projection and at-issueness
+# preprocessing
+
+# set working directory to directory of script
+this.dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(this.dir)
+
+source('../../helpers.R')
+
+
+# load required packages for pre-processing data
+library(tidyverse)
+theme_set(theme_bw())
+
+# read in the raw data
+d = read_csv("../data/experiment-trials.csv")
+
+# read in the subject information
+ds = read_csv("../data/experiment-subject_information.csv")
+
+# merge subject information into data
+d = d %>%
+  left_join(ds, by=c("workerid"))
+
+# how many participants?
+length(unique(d$workerid)) #300
+
+# look at Turkers' comments
+unique(ds$comments)
+
+# participant info
+table(d$age) #18-74
+length(which(is.na(d$age))) # 0 missing values
+median(d$age,na.rm=TRUE) #31
+
+d %>% 
+  select(gender, workerid) %>% 
+  unique() %>% 
+  group_by(gender) %>% 
+  summarize(count=n())
+#150 female, 145 male, 5 other, 0 undeclared
+
+# change the response for ai condition so that what was 0/not-at-issue is now 1/not-at-issue
+# by subtracting the ai responses from 1
+table(d$question_type,d$response)
+d[d$question_type == "ai",]$response = 1 - d[d$question_type == "ai",]$response
+
+# make a trial number
+unique(d$slide_number_in_experiment) #slide numbers from 5 to 57
+d$trial = d$slide_number_in_experiment - 4
+unique(d$trial) # trial numbers from 1 to 53 (27 missing because instruction)
+d[d$trial > 26,]$trial = d[d$trial > 26,]$trial - 1
+unique(d$trial) # trials from 1 to 52
+
+### exclude non-English speakers and non-American English speakers
+# exclude non-English speakers
+length(which(is.na(ds$language))) #no missing responses
+table(ds$language) 
+
+
+d <- d %>%
+  filter(language != "Spanish" & language != "Vietnamese") %>%  droplevels()
+length(unique(d$workerid)) # (data from 5 Turker excluded, 295 remaining Turkers)
+
+# exclude non-American English speakers
+length(unique(d$workerid))# 295
+length(which(is.na(d$american))) #1 person didn't respond
+table(d$american) 
+d <- d %>%
+  filter(d$american == "Yes") %>%  droplevels()
+length(unique(d$workerid)) #292 (3 Turkers excluded)
+
+# exclude Turkers based on main clause controls
+
+# main clauses
+names(d)
+d.MC <- d %>%
+  filter(short_trigger == "MC") %>%
+  droplevels()
+nrow(d.MC) #3504 (292 Turkers x 6 MCs x 2 questions)
+
+# projection of main clause data
+table(d$question_type)
+d.MC.Proj <- d.MC %>%
+  filter(question_type == "projective") %>%
+  droplevels()
+nrow(d.MC.Proj) #1752 (292 Turkers x 6 controls)
+
+# group projection mean (all Turkers, all clauses)
+round(mean(d.MC.Proj$response),2) #.95 (because speaker is committed to content of positive indicative sentences)
+
+# calculate each Turkers mean response to the projection of main clauses
+p.means = d.MC.Proj %>%
+  group_by(workerid) %>%
+  summarize(Mean = mean(response), CI.Low=ci.low(response), CI.High=ci.high(response)) %>%
+  ungroup() %>%
+  mutate(YMin = Mean-CI.Low, YMax = Mean+CI.High)
+
+ggplot(p.means, aes(x=workerid,y=Mean)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=YMin, ymax=YMax))+
+  geom_text(aes(label=workerid), vjust = 1, cex= 5)+
+  ylab("Projection response mean")
+
+# ai of main clause data
+d.MC.AI <- d.MC %>%
+  filter(question_type == "ai") %>%
+  droplevels()
+nrow(d.MC.AI) #1752
+
+# group not-at-issueness mean (all Turkers, all clauses)
+round(mean(d.MC.AI$response),2) #.04 (because main clause content is at-issue, coded as 0)
+
+# calculate each Turkers mean response to the projection of main clauses
+ai.means = d.MC.AI %>%
+  group_by(workerid) %>%
+  summarize(Mean = mean(response), CI.Low=ci.low(response), CI.High=ci.high(response)) %>%
+  ungroup() %>%
+  mutate(YMin = Mean-CI.Low, YMax = Mean+CI.High)
+
+ggplot(ai.means, aes(x=workerid,y=Mean)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=YMin, ymax=YMax))+
+  geom_text(aes(label=workerid), vjust = 1, cex= 5)+
+  ylab("NAI response mean")
+
+# look at Turkers whose response mean on projection and ainess of main clauses is more than 2
+# standard deviations away from the overall mean
+
+# get the Turkers who are more than 2 standard deviations below the mean on projection 
+p <- p.means[p.means$Mean < (mean(p.means$Mean) - 2*sd(p.means$Mean)),]
+p
+
+# get the Turkers who are more than 2 standard deviations above the mean on ai 
+ai <- ai.means[ai.means$Mean > (mean(ai.means$Mean) + 2*sd(ai.means$Mean)),]
+ai
+
+# look at the main clauses that these "outlier" Turkers did
+# make data subset of just the outliers
+outliers <- d.MC %>%
+  filter(workerid %in% p$workerid | workerid %in% ai$workerid)
+outliers = droplevels(outliers)
+length(unique(outliers$workerid)) #17 outlier Turkers
+
+# exclude all outliers identified above
+d <- d %>%
+  filter(!(workerid %in% p$workerid | workerid %in% ai$workerid)) %>%
+  droplevels()
+length(unique(d$workerid)) # 275 remaining Turkers (17 Turkers excluded)
+
+# exclude turkers who always clicked on roughly the same point on the scale 
+# ie turkers whose variance in overall response distribution is more 
+# than 2 sd below mean by-participant variance
+variances = d %>%
+  filter(short_trigger != "MC") %>%
+  droplevels() %>%
+  group_by(workerid) %>%
+  summarize(Variance = var(response)) %>%
+  mutate(TooSmall = Variance < mean(Variance) - 2*sd(Variance))
+
+lowvarworkers = as.character(variances[variances$TooSmall,]$workerid)
+summary(variances)
+lowvarworkers # 1 turker consistently clicked on roughly the same point on the scale
+
+lvw = d %>%
+  filter(short_trigger != "MC") %>%
+  filter(as.character(workerid) %in% lowvarworkers) %>%
+  droplevels() %>%
+  mutate(Participant = as.factor(as.character(workerid)))
+
+ggplot(lvw,aes(x=Participant,y=response)) +
+  geom_point()
+
+# exclude the participant identified above
+d <- droplevels(subset(d, !(d$workerid %in% lowvarworkers)))
+length(unique(d$workerid)) #274 Turkers remain
+
+# age and gender of remaining participants
+table(d$age) #18-74
+length(which(is.na(d$age))) # 0 missing values
+median(d$age,na.rm=TRUE) #31
+
+d %>% 
+  select(gender, workerid) %>% 
+  unique() %>% 
+  group_by(gender) %>% 
+  summarize(count=n())
+#141 female, 128 male, 5 other, 0 undeclared
+
+# write cleaned dataset to file
+write.csv(d, file="../data/data_preprocessed.csv",row.names=F,quote=F,sep =",",)
+nrow(d)
+
---
+++ results/main/meta-analysis/rscripts/analysis.R
@@ -0,0 +1,219 @@
+# meta-analyses
+# projection: comparing embedding under polar questions, negation, "perhaps", conditionals
+# at-issueness diagnostics
+
+# set working directory to directory of script
+this.dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(this.dir)
+
+source('../../helpers.R')
+
+# load required packages
+require(tidyverse)
+library(ggrepel)
+library(lmerTest)
+
+# read in the data
+polar = read.csv(file="../../1_projaiQ/data/data_preprocessed.csv") %>%
+  select(workerid,content,short_trigger,question_type,response) %>%
+  filter(short_trigger != "MC") %>%
+  spread(question_type,response) %>%
+  mutate(SubExperiment="polar")
+
+neg = read.csv(file="../../2_projaiN/data/data_preprocessed.csv") %>%
+  select(workerid,content,short_trigger,question_type,response) %>%
+  filter(short_trigger != "MC") %>%
+  spread(question_type,response) %>%
+  mutate(SubExperiment="neg")
+
+modal = read.csv(file="../../3_projaiM/data/data_preprocessed.csv") %>%
+  select(workerid,content,short_trigger,question_type,response) %>%
+  filter(short_trigger != "MC") %>%
+  spread(question_type,response) %>%
+  mutate(SubExperiment="modal")
+
+nrow(polar)
+nrow(neg)
+nrow(modal)
+
+# aggregate
+
+polar_a = polar %>%
+  group_by(short_trigger) %>%
+  summarise(AImean_P = mean(ai), ci.low_AI_P=ci.low(ai), ci.high_AI_P=ci.high(ai),
+            Projmean_P = mean(projective), ci.low_Proj_P=ci.low(projective), ci.high_Proj_P=ci.high(projective))
+  
+
+neg_a = neg %>%
+  group_by(short_trigger) %>%
+  summarise(AImean_N = mean(ai), ci.low_AI_N=ci.low(ai), ci.high_AI_N=ci.high(ai),
+            Projmean_N = mean(projective), ci.low_Proj_N=ci.low(projective), ci.high_Proj_N=ci.high(projective))
+
+modal_a = modal %>%
+  group_by(short_trigger) %>%
+  summarise(AImean_M = mean(ai), ci.low_AI_M=ci.low(ai), ci.high_AI_M=ci.high(ai),
+            Projmean_M = mean(projective), ci.low_Proj_M=ci.low(projective), ci.high_Proj_M=ci.high(projective))
+
+d = polar_a %>%
+  inner_join(neg_a,by=("short_trigger")) %>%
+  inner_join(modal_a,by=("short_trigger"))
+View(d)
+names(d)
+
+# correlations
+# projection
+cor(d$Projmean_P,d$Projmean_N) # .72
+cor(d$Projmean_P,d$Projmean_M) # .85
+cor(d$Projmean_N,d$Projmean_M) # .82
+
+# not-at-issueness
+cor(d$AImean_P,d$AImean_N) # .59
+cor(d$AImean_P,d$AImean_M) # .47
+cor(d$AImean_N,d$AImean_M) # .50
+
+
+# plot projection against projection ----
+ggplot(d, aes(x=Projmean_P,y=Projmean_N)) +
+  geom_abline(intercept=0,slope=1, color="gray", linetype="dashed") +
+  geom_text_repel(aes(label=short_trigger),nudge_x=-.05,size=4,show.legend=F) +
+  #geom_errorbar(aes(ymin=YMin,ymax=YMax),alpha=.8,color="gray") +
+  #geom_errorbarh(aes(xmin=XMin,xmax=XMax),alpha=.8,color="gray") +
+  geom_point() +
+  xlab("Mean certainty rating polar") +
+  ylab("Mean certainty rating negation") +
+  xlim(0,1) +
+  ylim(0,1) +
+  theme(legend.position = "top",axis.title = element_text(size=14),legend.text = element_text(size=14),legend.title=element_text(size=14))
+ggsave("../graphs/projection-polar-against-negation.pdf",width=6,height=6)
+
+ggplot(d, aes(x=Projmean_P,y=Projmean_M)) +
+  geom_abline(intercept=0,slope=1, color="gray", linetype="dashed") +
+  geom_text_repel(aes(label=short_trigger),nudge_x=-.05,size=4,show.legend=F) +
+  #geom_errorbar(aes(ymin=YMin,ymax=YMax),alpha=.8,color="gray") +
+  #geom_errorbarh(aes(xmin=XMin,xmax=XMax),alpha=.8,color="gray") +
+  geom_point() +
+  xlab("Mean certainty rating polar") +
+  ylab("Mean certainty rating modal") +
+  xlim(0,1) +
+  ylim(0,1) +
+  theme(legend.position = "top",axis.title = element_text(size=14),legend.text = element_text(size=14),legend.title=element_text(size=14))
+ggsave("../graphs/projection-polar-against-modal.pdf",width=6,height=6)
+
+ggplot(d, aes(x=Projmean_N,y=Projmean_M)) +
+  geom_abline(intercept=0,slope=1, color="gray", linetype="dashed") +
+  geom_text_repel(aes(label=short_trigger),nudge_x=-.05,size=4,show.legend=F) +
+  #geom_errorbar(aes(ymin=YMin,ymax=YMax),alpha=.8,color="gray") +
+  #geom_errorbarh(aes(xmin=XMin,xmax=XMax),alpha=.8,color="gray") +
+  geom_point() +
+  xlab("Mean certainty rating negation") +
+  ylab("Mean certainty rating modal") +
+  xlim(0,1) +
+  ylim(0,1) +
+  theme(legend.position = "top",axis.title = element_text(size=14),legend.text = element_text(size=14),legend.title=element_text(size=14))
+ggsave("../graphs/projection-negation-against-modal.pdf",width=6,height=6)
+
+# plot ai against ai ----
+ggplot(d, aes(x=AImean_P,y=AImean_N)) +
+  geom_abline(intercept=0,slope=1, color="gray", linetype="dashed") +
+  geom_text_repel(aes(label=short_trigger),nudge_x=-.05,size=4,show.legend=F) +
+  #geom_errorbar(aes(ymin=YMin,ymax=YMax),alpha=.8,color="gray") +
+  #geom_errorbarh(aes(xmin=XMin,xmax=XMax),alpha=.8,color="gray") +
+  geom_point() +
+  xlab("Mean not-at-issueness rating polar (asking whether)") +
+  ylab("Mean not-at-issueness rating negation (sure that)") +
+  xlim(0,1) +
+  ylim(0,1) +
+  theme(legend.position = "top",axis.title = element_text(size=14),legend.text = element_text(size=14),legend.title=element_text(size=14))
+ggsave("../graphs/nai-polar-against-negation.pdf",width=6,height=6)
+
+ggplot(d, aes(x=AImean_P,y=AImean_M)) +
+  geom_abline(intercept=0,slope=1, color="gray", linetype="dashed") +
+  geom_text_repel(aes(label=short_trigger),nudge_x=-.05,size=4,show.legend=F) +
+  #geom_errorbar(aes(ymin=YMin,ymax=YMax),alpha=.8,color="gray") +
+  #geom_errorbarh(aes(xmin=XMin,xmax=XMax),alpha=.8,color="gray") +
+  geom_point() +
+  xlab("Mean not-at-issueness rating polar (asking whether)") +
+  ylab("Mean not-at-issueness rating modal (sure that)") +
+  xlim(0,1) +
+  ylim(0,1) +
+  theme(legend.position = "top",axis.title = element_text(size=14),legend.text = element_text(size=14),legend.title=element_text(size=14))
+ggsave("../graphs/nai-polar-against-modal.pdf",width=6,height=6)
+
+ggplot(d, aes(x=AImean_N,y=AImean_M)) +
+  geom_abline(intercept=0,slope=1, color="gray", linetype="dashed") +
+  geom_text_repel(aes(label=short_trigger),nudge_x=-.05,size=4,show.legend=F) +
+  #geom_errorbar(aes(ymin=YMin,ymax=YMax),alpha=.8,color="gray") +
+  #geom_errorbarh(aes(xmin=XMin,xmax=XMax),alpha=.8,color="gray") +
+  geom_point() +
+  xlab("Mean not-at-issueness rating negation (sure that)") +
+  ylab("Mean not-at-issueness rating modal (sure that)") +
+  xlim(0,1) +
+  ylim(0,1) +
+  theme(legend.position = "top",axis.title = element_text(size=14),legend.text = element_text(size=14),legend.title=element_text(size=14))
+ggsave("../graphs/nai-negation-against-modal.pdf",width=6,height=6)
+
+######### HAVENT INTEGRATED ERROR BARS YET
+d = agr1 %>%
+  inner_join(agr2,by=c("short_trigger","SubExperiment"))
+nrow(d)
+d = as.data.frame(d) %>%
+  filter(short_trigger != "MC") %>%
+  mutate(YMin=mean_exp2 - ci.low_exp2, YMax=mean_exp2 + ci.high_exp2, XMin=mean_exp1 - ci.low_exp1, XMax=mean_exp1+ci.high_exp1)
+
+# correlations overall and by sub-experiment
+cor(d$mean_exp1,d$mean_exp2) # .62
+d %>%
+  group_by(SubExperiment) %>%
+  summarise(r=cor(mean_exp1,mean_exp2))
+
+cbPalette <- c("#0072B2", "#D55E00")
+
+# figure 11
+ggplot(d, aes(x=mean_exp1,y=mean_exp2,color=SubExperiment)) +
+  geom_abline(intercept=0,slope=1, color="gray", linetype="dashed") +
+  geom_text_repel(aes(label=short_trigger),nudge_x=-.05,size=4,show.legend=F) +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),alpha=.8,color="gray") +
+  geom_errorbarh(aes(xmin=XMin,xmax=XMax),alpha=.8,color="gray") +
+  geom_point() +
+  scale_color_manual(values=cbPalette,name="Sub-experiment") +
+  xlab("Mean not-at-issueness ('asking whether', Exp. 1)") +
+  ylab("Mean not-at-issueness ('are you sure', Exp. 2)") +
+  xlim(.3,1) +
+  ylim(.3,1) +
+  theme(legend.position = "top",axis.title = element_text(size=14),legend.text = element_text(size=14),legend.title=element_text(size=14))
+ggsave("graphs/correlation-bytrigger.pdf",width=6,height=6)
+
+
+# aggregate by both trigger and content
+agr1 = exp1 %>%
+  group_by(short_trigger,content,SubExperiment) %>%
+  summarise(mean_exp1 = mean(ai), ci.low_exp1=ci.low(ai), ci.high_exp1=ci.high(ai))
+agr2 = exp2 %>%
+  group_by(short_trigger,content,SubExperiment) %>%
+  summarise(mean_exp2 = mean(response), ci.low_exp2=ci.low(response), ci.high_exp2=ci.high(response))
+
+d = agr1 %>%
+  inner_join(agr2,by=c("short_trigger","content","SubExperiment"))
+nrow(d)
+d = as.data.frame(d) %>%
+  filter(short_trigger != "MC") %>%
+  mutate(YMin=mean_exp2 - ci.low_exp2, YMax=mean_exp2 + ci.high_exp2, XMin=mean_exp1 - ci.low_exp1, XMax=mean_exp1+ci.high_exp1)
+
+# correlations overall and by sub-experiment
+cor(d$mean_exp1,d$mean_exp2) # .31
+d %>%
+  group_by(SubExperiment) %>%
+  summarise(r=cor(mean_exp1,mean_exp2))
+
+d$cmean_exp1 = myCenter(d$mean_exp1)
+d$SubExp = ifelse(d$SubExperiment == "a",0,1)
+d$cSubExperiment = myCenter(d$SubExp)
+
+# how much does exp 1 measure explain exp 2 measure? (footnote 13)
+m = lmer(mean_exp2 ~ 1+cmean_exp1 * cSubExperiment + (1|content), data = d)
+summary(m)
+
+m.simple = lmer(mean_exp2 ~ 1+cmean_exp1 * SubExperiment - cmean_exp1 + (1|content), data = d)
+summary(m.simple)
+
+
