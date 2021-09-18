42ed4c2a36f37d055d1b3afeff50d0ff3c014d90
judith-tonhauser <tonhauser.1@osu.edu>
2021-09-18 09:47:48 +0200

    worked on moscow talk

---
+++ results/main/9_projaiQ/rscripts/preprocessing.R
@@ -0,0 +1,160 @@
+# 9_projaiQ
+# projection and at-issueness (assent with adversative continuation)
+# preprocessing
+
+# set working directory to directory of script
+this.dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(this.dir)
+
+source('../../helpers.R')
+theme_set(theme_bw())
+
+# load required packages for pre-processing data
+require(tidyverse)
+
+# read in the raw data
+d = read_csv("../data/experiment-trials.csv")
+nrow(d) #13000 / 250 = 52 trials (the experiment was done 250 times, as planned)
+head(d)
+summary(d)
+
+length(unique(d$workerid)) #250
+
+# read in the subject information
+ds = read_csv("../data/experiment-subject_information.csv")
+length(unique(ds$workerid)) #250
+nrow(ds) #250
+head(ds)
+summary(d) 
+
+# look at participants' comments
+unique(ds$comments)
+
+# merge subject information into data
+d = d %>%
+  left_join(ds, by=c("workerid"))
+
+# participant info
+table(d$age) #18-66
+length(which(is.na(d$age))) #52 missing values
+median(d$age,na.rm=TRUE) #30
+
+d %>% 
+  select(gender, workerid) %>% 
+  unique() %>% 
+  group_by(gender) %>% 
+  summarize(count=n())
+#140 female, 102 male, 7 other, 1 undeclared
+
+# change the response for ai condition so that what was 0/not-at-issue is now 1/not-at-issue
+# by subtracting the ai responses from 1
+table(d$question_type,d$response)
+d[d$question_type == "ai",]$response = 1 - d[d$question_type == "ai",]$response
+
+# make a trial number
+unique(d$slide_number_in_experiment) #slide numbers from 5 to 57
+d$trial = d$slide_number_in_experiment - 4
+unique(d$trial) # trial numbers from 1 to 53 (27 missing because instruction)
+d[d$trial > 26,]$trial = d[d$trial > 26,]$trial - 1
+unique(d$trial) # trials from 1 to 52
+
+### exclude non-American English speakers
+length(unique(d$workerid)) #250
+length(which(is.na(d$language))) #156 missing responses
+table(d$language) 
+
+# exclude anybody who didn't include English among languages spoken
+d <- d %>%
+  filter(language != "Spanish") %>%
+  droplevels()
+length(unique(d$workerid)) #246 (data from 3 participant excluded)
+
+# exclude non-American English speakers
+length(unique(d$workerid))# 246
+length(which(is.na(d$american))) #0
+table(d$american) 
+
+d <- d %>%
+  filter(american == "Yes") %>%
+  droplevels()
+length(unique(d$workerid)) #245 (data from 1 participants excluded)
+
+# exclude participants based on main clause controls
+
+# main clauses
+names(d)
+d.MC <- d %>%
+  filter(short_trigger == "MC") %>%
+  droplevels()
+nrow(d.MC) #2940 / 245 participants = 12 (6 main clause controls in each of the two blocks)
+
+# projection of main clause data
+table(d$question_type)
+d.MC.Proj <- d.MC %>%
+  filter(question_type == "projective") %>%
+  droplevels()
+nrow(d.MC.Proj) #1470
+
+# group projection mean (all participants, all clauses)
+round(mean(d.MC.Proj$response),2) #.17
+
+# calculate each participants mean response to the projection of main clauses
+p.means = d.MC.Proj %>%
+  group_by(workerid) %>%
+  summarize(Mean = mean(response), CI.Low=ci.low(response), CI.High=ci.high(response)) %>%
+  ungroup() %>%
+  mutate(YMin = Mean-CI.Low, YMax = Mean+CI.High)
+
+ggplot(p.means, aes(x=workerid,y=Mean)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=YMin, ymax=YMax))+
+  geom_text(aes(label=workerid), vjust = 1, cex= 5)+
+  ylab("Projection response mean")
+
+# ai of main clause data
+d.MC.AI <- d.MC %>%
+  filter(question_type == "ai") %>%
+  droplevels()
+nrow(d.MC.AI) #1470
+
+# group not-at-issueness mean (all participants, all clauses)
+round(mean(d.MC.AI$response),2) #.28
+
+# calculate each participants mean response to the projection of main clauses
+ai.means = d.MC.AI %>%
+  group_by(workerid) %>%
+  summarize(Mean = mean(response), CI.Low=ci.low(response), CI.High=ci.high(response)) %>%
+  ungroup() %>%
+  mutate(YMin = Mean-CI.Low, YMax = Mean+CI.High)
+
+ggplot(ai.means, aes(x=workerid,y=Mean)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=YMin, ymax=YMax))+
+  geom_text(aes(label=workerid), vjust = 1, cex= 5)+
+  ylab("NAI response mean")
+
+# look at participants whose response mean on projection and ainess of main clauses is more than 2
+# standard deviations away from the overall mean
+
+# get the participants who are more than 2 standard deviations above the mean on projection 
+p <- p.means[p.means$Mean > (mean(p.means$Mean) + 2*sd(p.means$Mean)),]
+p #13 participants 
+
+# get the participants who are more than 2 standard deviations above the mean on ai 
+ai <- ai.means[ai.means$Mean > (mean(ai.means$Mean) + 2*sd(ai.means$Mean)),]
+ai #10 participants
+
+# make data subset of just the outliers
+outliers <- d.MC %>%
+  filter(workerid %in% p$workerid | workerid %in% ai$workerid)
+outliers = droplevels(outliers)
+nrow(outliers) #240 / 12 = 20 outlier participants
+
+# exclude all outlier participants identified above
+d <- d %>%
+  filter(!(workerid %in% p$workerid | workerid %in% ai$workerid)) %>%
+  droplevels()
+length(unique(d$workerid)) # 225 remaining participants (20 participants excluded)
+
+# write cleaned dataset to file
+write_csv(d, path="../data/data_preprocessed.csv")
