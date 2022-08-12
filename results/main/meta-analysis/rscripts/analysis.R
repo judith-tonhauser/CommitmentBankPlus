67598b2c00d946719c58fc154439e0bc6eed7139
judith-tonhauser <tonhauser.1@osu.edu>
2022-08-12 14:46:14 +0200

    changed meta analysis file

--- results/main/meta-analysis/rscripts/analysis.R
+++ results/main/meta-analysis/rscripts/analysis.R
@@ -16,125 +16,194 @@ library(lmerTest)
 # read in the data
 
 # at-issueness diagnostics: Q: asking whether, N/M/C: sure that
-polar_1 = read.csv(file="../../1_projaiQ/data/data_preprocessed.csv") %>%
+exp1q = read.csv(file="../../1_projaiQ/data/data_preprocessed.csv") %>%
   select(workerid,content,short_trigger,question_type,response) %>%
   filter(short_trigger != "MC") %>%
   spread(question_type,response) %>%
-  mutate(SubExperiment="polar_1")
+  mutate(SubExperiment="exp1q")
 
-neg_2 = read.csv(file="../../2_projaiN/data/data_preprocessed.csv") %>%
+exp1n = read.csv(file="../../2_projaiN/data/data_preprocessed.csv") %>%
   select(workerid,content,short_trigger,question_type,response) %>%
   filter(short_trigger != "MC") %>%
   spread(question_type,response) %>%
-  mutate(SubExperiment="neg_2")
+  mutate(SubExperiment="exp1n")
 
-modal_3 = read.csv(file="../../3_projaiM/data/data_preprocessed.csv") %>%
+exp1m = read.csv(file="../../3_projaiM/data/data_preprocessed.csv") %>%
   select(workerid,content,short_trigger,question_type,response) %>%
   filter(short_trigger != "MC") %>%
   spread(question_type,response) %>%
-  mutate(SubExperiment="modal_3")
+  mutate(SubExperiment="exp1m")
 
-cond_4 = read.csv(file="../../4_projaiC/data/data_preprocessed.csv") %>%
+exp1c = read.csv(file="../../4_projaiC/data/data_preprocessed.csv") %>%
   select(workerid,content,short_trigger,question_type,response) %>%
   filter(short_trigger != "MC") %>%
   spread(question_type,response) %>%
-  mutate(SubExperiment="conditional_4")
+  mutate(SubExperiment="exp1c")
 
 # at-issueness diagnostic: assent with positive continuation
 
-polar_5 = read.csv(file="../../5_projaiQ/data/data_preprocessed.csv") %>%
+exp2q = read.csv(file="../../5_projaiQ/data/data_preprocessed.csv") %>%
   select(workerid,content,short_trigger,question_type,response) %>%
   filter(short_trigger != "MC") %>%
   spread(question_type,response) %>%
-  mutate(SubExperiment="polar_5")
+  mutate(SubExperiment="exp2q")
 
-neg_6 = read.csv(file="../../6_projaiN/data/data_preprocessed.csv") %>%
+exp2n = read.csv(file="../../6_projaiN/data/data_preprocessed.csv") %>%
   select(workerid,content,short_trigger,question_type,response) %>%
   filter(short_trigger != "MC") %>%
   spread(question_type,response) %>%
-  mutate(SubExperiment="neg_6")
+  mutate(SubExperiment="exp2n")
 
-modal_7 = read.csv(file="../../7_projaiM/data/data_preprocessed.csv") %>%
+exp2m = read.csv(file="../../7_projaiM/data/data_preprocessed.csv") %>%
   select(workerid,content,short_trigger,question_type,response) %>%
   filter(short_trigger != "MC") %>%
   spread(question_type,response) %>%
-  mutate(SubExperiment="modal_7")
+  mutate(SubExperiment="exp2m")
 
-cond_8 = read.csv(file="../../8_projaiC/data/data_preprocessed.csv") %>%
+exp2c = read.csv(file="../../8_projaiC/data/data_preprocessed.csv") %>%
   select(workerid,content,short_trigger,question_type,response) %>%
   filter(short_trigger != "MC") %>%
   spread(question_type,response) %>%
-  mutate(SubExperiment="conditional_8")
+  mutate(SubExperiment="exp2c")
 
 # at-issueness diagnostic: assent with adversative continuation
 
-polar_9 = read.csv(file="../../9_projaiQ/data/data_preprocessed.csv") %>%
+exp3q = read.csv(file="../../9_projaiQ/data/data_preprocessed.csv") %>%
   select(workerid,content,short_trigger,question_type,response) %>%
   filter(short_trigger != "MC") %>%
   spread(question_type,response) %>%
-  mutate(SubExperiment="polar_9")
+  mutate(SubExperiment="exp3q")
 
-neg_10 = read.csv(file="../../10_projaiN/data/data_preprocessed.csv") %>%
+exp3n = read.csv(file="../../10_projaiN/data/data_preprocessed.csv") %>%
   select(workerid,content,short_trigger,question_type,response) %>%
   filter(short_trigger != "MC") %>%
   spread(question_type,response) %>%
-  mutate(SubExperiment="neg_10")
+  mutate(SubExperiment="exp3n")
 
-modal_11 = read.csv(file="../../11_projaiM/data/data_preprocessed.csv") %>%
+exp3m = read.csv(file="../../11_projaiM/data/data_preprocessed.csv") %>%
   select(workerid,content,short_trigger,question_type,response) %>%
   filter(short_trigger != "MC") %>%
   spread(question_type,response) %>%
-  mutate(SubExperiment="modal_11")
+  mutate(SubExperiment="exp3m")
 
-cond_12 = read.csv(file="../../12_projaiC/data/data_preprocessed.csv") %>%
+exp3c = read.csv(file="../../12_projaiC/data/data_preprocessed.csv") %>%
   select(workerid,content,short_trigger,question_type,response) %>%
   filter(short_trigger != "MC") %>%
   spread(question_type,response) %>%
-  mutate(SubExperiment="conditional_12")
+  mutate(SubExperiment="exp3c")
 
 # aggregate
 
-polar_1a = polar_1 %>%
+exp1qA = exp1q %>%
   group_by(short_trigger) %>%
-  summarise(AImean_P1 = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
-            Projmean_P1 = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
-  mutate(YMin_AI_P1 = AImean_P1 - ci.low_AI, YMax_AI_P1 = AImean_P1 + ci.high_AI, 
-         YMin_Proj_P1 = Projmean_P1 - ci.low_Proj, YMax_Proj_P1 = Projmean_P1 + ci.high_Proj) %>%
+  summarise(AImean_exp1q = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
+            Projmean_exp1q = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
+  mutate(YMin_AI_exp1q = AImean_exp1q - ci.low_AI, YMax_AI_exp1q = AImean_exp1q + ci.high_AI, 
+         YMin_Proj_exp1q = Projmean_exp1q - ci.low_Proj, YMax_Proj_exp1q = Projmean_exp1q + ci.high_Proj) %>%
   select(-c(ci.low_AI,ci.high_AI,ci.low_Proj,ci.high_Proj))
-#View(polar_1a)
+#View(exp1qA)
 
-polar_5a = polar_5 %>%
+exp1nA = exp1n %>%
   group_by(short_trigger) %>%
-  summarise(AImean_P5 = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
-            Projmean_P5 = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
-  mutate(YMin_AI_P5 = AImean_P5 - ci.low_AI, YMax_AI_P5 = AImean_P5 + ci.high_AI, 
-         YMin_Proj_P5 = Projmean_P5 - ci.low_Proj, YMax_Proj_P5 = Projmean_P5 + ci.high_Proj) %>%
+  summarise(AImean_exp1n = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
+            Projmean_exp1n = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
+  mutate(YMin_AI_exp1n = AImean_exp1n - ci.low_AI, YMax_AI_exp1n = AImean_exp1n + ci.high_AI, 
+         YMin_Proj_exp1n = Projmean_exp1n - ci.low_Proj, YMax_Proj_exp1n = Projmean_exp1n + ci.high_Proj) %>%
   select(-c(ci.low_AI,ci.high_AI,ci.low_Proj,ci.high_Proj))
 
-
-neg_6a = neg_6 %>%
+exp1mA = exp1m %>%
   group_by(short_trigger) %>%
-  summarise(AImean_N6 = mean(ai), ci.low_AI_N=ci.low(ai), ci.high_AI_N=ci.high(ai),
-            Projmean_N6 = mean(projective), ci.low_Proj_N=ci.low(projective), ci.high_Proj_N=ci.high(projective)) %>%
-  mutate(YMin_AI_N6 = AImean_N6 - ci.low_AI_N, YMax_AI_N6 = AImean_N6 + ci.high_AI_N, 
-         YMin_Proj_N6 = Projmean_N6 - ci.low_Proj_N, YMax_Proj_N6 = Projmean_N6 + ci.high_Proj_N) %>%
-  select(-c(ci.low_AI_N,ci.high_AI_N,ci.low_Proj_N,ci.high_Proj_N))
+  summarise(AImean_exp1m = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
+            Projmean_exp1m = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
+  mutate(YMin_AI_exp1m = AImean_exp1m - ci.low_AI, YMax_AI_exp1m = AImean_exp1m + ci.high_AI, 
+         YMin_Proj_exp1m = Projmean_exp1m - ci.low_Proj, YMax_Proj_exp1m = Projmean_exp1m + ci.high_Proj) %>%
+  select(-c(ci.low_AI,ci.high_AI,ci.low_Proj,ci.high_Proj))
 
+exp1cA = exp1c %>%
+  group_by(short_trigger) %>%
+  summarise(AImean_exp1c = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
+            Projmean_exp1c = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
+  mutate(YMin_AI_exp1c = AImean_exp1c - ci.low_AI, YMax_AI_exp1c = AImean_exp1c + ci.high_AI, 
+         YMin_Proj_exp1c = Projmean_exp1c - ci.low_Proj, YMax_Proj_exp1c = Projmean_exp1c + ci.high_Proj) %>%
+  select(-c(ci.low_AI,ci.high_AI,ci.low_Proj,ci.high_Proj))
 
-d = polar_1a %>%
-#  inner_join(neg_2a,by=("short_trigger")) %>%
-#  inner_join(modal_3a,by=("short_trigger")) %>%
-#  inner_join(cond_4a,by=("short_trigger")) %>%
-  inner_join(polar_5a,by=("short_trigger")) %>%
-  inner_join(neg_6a,by=("short_trigger")) %>%
-#  inner_join(modal_7a,by=("short_trigger")) %>%
-#  inner_join(cond_8a,by=("short_trigger")) %>%
-#  inner_join(polar_9a,by=("short_trigger")) %>%
-#  inner_join(neg_10a,by=("short_trigger")) 
-#%>%
-#inner_join(modal_7a,by=("short_trigger")) %>%
-#inner_join(cond_8a,by=("short_trigger"))
+exp2qA = exp2q %>%
+  group_by(short_trigger) %>%
+  summarise(AImean_exp2q = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
+            Projmean_exp2q = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
+  mutate(YMin_AI_exp2q = AImean_exp2q - ci.low_AI, YMax_AI_exp2q = AImean_exp2q + ci.high_AI, 
+         YMin_Proj_exp2q = Projmean_exp2q - ci.low_Proj, YMax_Proj_exp2q = Projmean_exp2q + ci.high_Proj) %>%
+  select(-c(ci.low_AI,ci.high_AI,ci.low_Proj,ci.high_Proj))
 
+exp2nA = exp2n %>%
+  group_by(short_trigger) %>%
+  summarise(AImean_exp2n = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
+            Projmean_exp2n = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
+  mutate(YMin_AI_exp2n = AImean_exp2n - ci.low_AI, YMax_AI_exp2n = AImean_exp2n + ci.high_AI, 
+         YMin_Proj_exp2n = Projmean_exp2n - ci.low_Proj, YMax_Proj_exp2n = Projmean_exp2n + ci.high_Proj) %>%
+  select(-c(ci.low_AI,ci.high_AI,ci.low_Proj,ci.high_Proj))
+
+exp2mA = exp2m %>%
+  group_by(short_trigger) %>%
+  summarise(AImean_exp2m = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
+            Projmean_exp2m = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
+  mutate(YMin_AI_exp2m = AImean_exp2m - ci.low_AI, YMax_AI_exp2m = AImean_exp2m + ci.high_AI, 
+         YMin_Proj_exp2m = Projmean_exp2m - ci.low_Proj, YMax_Proj_exp2m = Projmean_exp2m + ci.high_Proj) %>%
+  select(-c(ci.low_AI,ci.high_AI,ci.low_Proj,ci.high_Proj))
+
+exp2cA = exp2c %>%
+  group_by(short_trigger) %>%
+  summarise(AImean_exp2c = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
+            Projmean_exp2c = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
+  mutate(YMin_AI_exp2c = AImean_exp2c - ci.low_AI, YMax_AI_exp2c = AImean_exp2c + ci.high_AI, 
+         YMin_Proj_exp2c = Projmean_exp2c - ci.low_Proj, YMax_Proj_exp2c = Projmean_exp2c + ci.high_Proj) %>%
+  select(-c(ci.low_AI,ci.high_AI,ci.low_Proj,ci.high_Proj))
+
+exp3qA = exp3q %>%
+  group_by(short_trigger) %>%
+  summarise(AImean_exp3q = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
+            Projmean_exp3q = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
+  mutate(YMin_AI_exp3q = AImean_exp3q - ci.low_AI, YMax_AI_exp3q = AImean_exp3q + ci.high_AI, 
+         YMin_Proj_exp3q = Projmean_exp3q - ci.low_Proj, YMax_Proj_exp3q = Projmean_exp3q + ci.high_Proj) %>%
+  select(-c(ci.low_AI,ci.high_AI,ci.low_Proj,ci.high_Proj))
+
+exp3nA = exp3n %>%
+  group_by(short_trigger) %>%
+  summarise(AImean_exp3n = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
+            Projmean_exp3n = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
+  mutate(YMin_AI_exp3n = AImean_exp3n - ci.low_AI, YMax_AI_exp3n = AImean_exp3n + ci.high_AI, 
+         YMin_Proj_exp3n = Projmean_exp3n - ci.low_Proj, YMax_Proj_exp3n = Projmean_exp3n + ci.high_Proj) %>%
+  select(-c(ci.low_AI,ci.high_AI,ci.low_Proj,ci.high_Proj))
+
+exp3mA = exp3m %>%
+  group_by(short_trigger) %>%
+  summarise(AImean_exp3m = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
+            Projmean_exp3m = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
+  mutate(YMin_AI_exp3m = AImean_exp3m - ci.low_AI, YMax_AI_exp3m = AImean_exp3m + ci.high_AI, 
+         YMin_Proj_exp3m = Projmean_exp3m - ci.low_Proj, YMax_Proj_exp3m = Projmean_exp3m + ci.high_Proj) %>%
+  select(-c(ci.low_AI,ci.high_AI,ci.low_Proj,ci.high_Proj))
+
+exp3cA = exp3c %>%
+  group_by(short_trigger) %>%
+  summarise(AImean_exp3c = mean(ai), ci.low_AI=ci.low(ai), ci.high_AI=ci.high(ai),
+            Projmean_exp3c = mean(projective), ci.low_Proj=ci.low(projective), ci.high_Proj=ci.high(projective)) %>%
+  mutate(YMin_AI_exp3c = AImean_exp3c - ci.low_AI, YMax_AI_exp3c = AImean_exp3c + ci.high_AI, 
+         YMin_Proj_exp3c = Projmean_exp3c - ci.low_Proj, YMax_Proj_exp3c = Projmean_exp3c + ci.high_Proj) %>%
+  select(-c(ci.low_AI,ci.high_AI,ci.low_Proj,ci.high_Proj))
+
+d = exp1qA %>%
+  inner_join(exp1nA,by=("short_trigger")) %>%
+  inner_join(exp1mA,by=("short_trigger")) %>%
+  inner_join(exp1cA,by=("short_trigger")) %>%
+  inner_join(exp2qA,by=("short_trigger")) %>%
+  inner_join(exp2nA,by=("short_trigger")) %>%
+  inner_join(exp2mA,by=("short_trigger")) %>%
+  inner_join(exp2cA,by=("short_trigger")) %>%
+  inner_join(exp3qA,by=("short_trigger")) %>%
+  inner_join(exp3nA,by=("short_trigger")) %>%
+  inner_join(exp3mA,by=("short_trigger")) %>%
+  inner_join(exp3cA,by=("short_trigger"))
+  
 summary(d)  
   
 # add VeridicalityGroup and colors for plotting
@@ -174,12 +243,37 @@ max(cond_12a$AImean_C12) - min(cond_12a$AImean_C12) #.
 ## correlations ----
 
 # projection correlations across embeddings (same not-at-issueness diagnostics)
-cor(d$Projmean_P1,d$Projmean_N2) # .72
-cor(d$Projmean_P1,d$Projmean_M3) # .85
-cor(d$Projmean_N2,d$Projmean_M3) # .82
-cor(d$Projmean_P1,d$Projmean_C4) # .62
-cor(d$Projmean_N2,d$Projmean_C4) # .54
-cor(d$Projmean_M3,d$Projmean_C4) # .73
+
+# exp1
+cor(d$Projmean_exp1q,d$Projmean_exp1n) # .95
+cor(d$Projmean_exp1q,d$Projmean_exp1m) # .92
+cor(d$Projmean_exp1q,d$Projmean_exp1c) # .96
+
+cor(d$Projmean_exp1n,d$Projmean_exp1m) # .81
+cor(d$Projmean_exp1n,d$Projmean_exp1c) # .88
+
+cor(d$Projmean_exp1m,d$Projmean_exp1c) # .94
+
+# exp2
+cor(d$Projmean_exp2q,d$Projmean_exp2n) # .93
+cor(d$Projmean_exp2q,d$Projmean_exp2m) # .92
+cor(d$Projmean_exp2q,d$Projmean_exp2c) # .94
+
+cor(d$Projmean_exp2n,d$Projmean_exp2m) # .81
+cor(d$Projmean_exp2n,d$Projmean_exp2c) # .87
+
+cor(d$Projmean_exp2m,d$Projmean_exp2c) # .94
+
+# exp3
+cor(d$Projmean_exp3q,d$Projmean_exp3n) # .95
+cor(d$Projmean_exp3q,d$Projmean_exp3m) # .92
+cor(d$Projmean_exp3q,d$Projmean_exp3c) # .94
+
+cor(d$Projmean_exp3n,d$Projmean_exp3m) # .81
+cor(d$Projmean_exp3n,d$Projmean_exp3c) # .84
+
+cor(d$Projmean_exp3m,d$Projmean_exp3c) # .94
+
 
 # for Moscow talk:
 cor(d$Projmean_P1,d$Projmean_N6) #.95
@@ -267,22 +361,58 @@ cor(d$Projmean_C12,d$AImean_C12) # .
 summary(d)
 d$Colors
 
+
 # plot projection against projection ----
-ggplot(d, aes(x=Projmean_P1,y=Projmean_P5,fill=VeridicalityGroup)) +
+ggplot(d, aes(x=Projmean_exp1n,y=Projmean_exp1m)) +
   geom_point(shape=21) +
-  scale_fill_manual(values=c("tomato1","black")) +
-  geom_errorbarh(aes(xmin=YMin_Proj_P1,xmax=YMax_Proj_P1),alpha=.8,color="gray") +
-  geom_errorbar(aes(ymin=YMin_Proj_P5,ymax=YMax_Proj_P5),alpha=.8,color="gray") +
+  #scale_fill_manual(values=c("tomato1","black")) +
+  geom_errorbarh(aes(xmin=YMin_Proj_exp1n,xmax=YMax_Proj_exp1n),alpha=.8,color="gray") +
+  geom_errorbar(aes(ymin=YMin_Proj_exp1m,ymax=YMax_Proj_exp1m),alpha=.8,color="gray") +
   geom_abline(intercept=0,slope=1, color="gray", linetype="dashed") +
-  geom_text_repel(aes(label=short_trigger,color=d$Colors),nudge_x=-.05,size=5,show.legend=F) +
-  scale_colour_manual(values=c("black", "tomato1")) +
-  xlab("Mean certainty rating (Exp 1, polar Q)") +
-  ylab("Mean certainty rating (Exp 2, polar Q)") +
+  geom_text_repel(aes(label=short_trigger,color=d$Colors),nudge_x=1,size=5,show.legend=F) +
+  #scale_colour_manual(values=c("black", "tomato1")) +
+  xlab("Mean certainty rating Exp 1n") +
+  ylab("Mean certainty rating Exp 1m") +
   scale_y_continuous(limits = c(0,1),breaks = c(0,0.2,0.4,0.6,0.8,1.0), labels= c("0",".2",".4",".6",".8","1")) +
   scale_x_continuous(limits = c(0,1),breaks = c(0,0.2,0.4,0.6,0.8,1.0), labels= c("0",".2",".4",".6",".8","1")) +
   coord_fixed(ratio = 1) +
   theme(legend.position = "none", axis.title.x = element_text(color="black", size=14), axis.title.y = element_text(color="black", size=14))
-ggsave("../graphs/projection-P1-against-P5.pdf",width=5,height=5)
+ggsave("../graphs/projection-exp1n-against-exp1m.pdf",width=5,height=5)
+
+ggplot(d, aes(x=Projmean_exp2n,y=Projmean_exp2m)) +
+  geom_point(shape=21) +
+  #scale_fill_manual(values=c("tomato1","black")) +
+  geom_errorbarh(aes(xmin=YMin_Proj_exp2n,xmax=YMax_Proj_exp2n),alpha=.8,color="gray") +
+  geom_errorbar(aes(ymin=YMin_Proj_exp2m,ymax=YMax_Proj_exp2m),alpha=.8,color="gray") +
+  geom_abline(intercept=0,slope=1, color="gray", linetype="dashed") +
+  geom_text_repel(aes(label=short_trigger,color=d$Colors),nudge_x=1,size=5,show.legend=F) +
+  #scale_colour_manual(values=c("black", "tomato1")) +
+  xlab("Mean certainty rating Exp 2n") +
+  ylab("Mean certainty rating Exp 2m") +
+  scale_y_continuous(limits = c(0,1),breaks = c(0,0.2,0.4,0.6,0.8,1.0), labels= c("0",".2",".4",".6",".8","1")) +
+  scale_x_continuous(limits = c(0,1),breaks = c(0,0.2,0.4,0.6,0.8,1.0), labels= c("0",".2",".4",".6",".8","1")) +
+  coord_fixed(ratio = 1) +
+  theme(legend.position = "none", axis.title.x = element_text(color="black", size=14), axis.title.y = element_text(color="black", size=14))
+ggsave("../graphs/projection-exp2n-against-exp2m.pdf",width=5,height=5)
+
+ggplot(d, aes(x=Projmean_exp2n,y=Projmean_exp1n)) +
+  geom_point(shape=21) +
+  #scale_fill_manual(values=c("tomato1","black")) +
+  geom_errorbarh(aes(xmin=YMin_Proj_exp2n,xmax=YMax_Proj_exp2n),alpha=.8,color="gray") +
+  geom_errorbar(aes(ymin=YMin_Proj_exp1n,ymax=YMax_Proj_exp1n),alpha=.8,color="gray") +
+  geom_abline(intercept=0,slope=1, color="gray", linetype="dashed") +
+  geom_text_repel(aes(label=short_trigger,color=d$Colors),nudge_x=1,size=5,show.legend=F) +
+  #scale_colour_manual(values=c("black", "tomato1")) +
+  xlab("Mean certainty rating Exp 2n") +
+  ylab("Mean certainty rating Exp 1n") +
+  scale_y_continuous(limits = c(0,1),breaks = c(0,0.2,0.4,0.6,0.8,1.0), labels= c("0",".2",".4",".6",".8","1")) +
+  scale_x_continuous(limits = c(0,1),breaks = c(0,0.2,0.4,0.6,0.8,1.0), labels= c("0",".2",".4",".6",".8","1")) +
+  coord_fixed(ratio = 1) +
+  theme(legend.position = "none", axis.title.x = element_text(color="black", size=14), axis.title.y = element_text(color="black", size=14))
+ggsave("../graphs/projection-exp2n-against-exp1n.pdf",width=5,height=5)
+
+
+########
 
 ggplot(d, aes(x=Projmean_P1,y=Projmean_N6,fill=VeridicalityGroup)) +
   geom_point(shape=21) +
