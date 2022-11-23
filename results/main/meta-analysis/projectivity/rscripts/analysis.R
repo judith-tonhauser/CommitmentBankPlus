3513688576f67e98b83d419e337d0fc6ab5f9d2d
lihofman <lisa.hofmann@ling.uni-stuttgart.de>
2022-11-23 08:45:27 +0100

    added analysis files

---
+++ results/main/meta-analysis/projectivity/rscripts/analysis.R
@@ -0,0 +1,56 @@
+
+### data analysis for projection data ----
+
+# set wd to script dir
+this.dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(this.dir)
+
+# load data
+data <- read.csv("../data/data_combined.csv", header = TRUE, sep = ",")
+
+
+# variables and contrasts for data analysis: ----
+## our dependent variable: projective (sliding-scale ratings of speaker certainty that complement clause is true)
+data$projective <- numeric(data$projective)
+
+# our independent variables
+## inference-triggering predicate, coded as "verb"
+data$verb <- as.factor(data$verb)
+levels(data$verb)
+contrasts(data$verb)
+
+### dummy coding with "suggest" as baseline
+data$verb <- relevel(data$verb, ref = "suggest")
+contrasts(data$verb)
+
+#### this was our choice for a baseline, because this is the verb with the 
+#### smallest differences between by-operator projectivity (ostensibly, based on graph)
+
+## embedding operator, coded as "op"
+data$op <- as.factor(data$op)
+levels(data$op)
+contrasts(data$op)
+### dummy coding with "c" (conditionals) as baseline
+
+# coding random effects as factors
+data$workerid <- as.factor(data$workerid)
+data$content <- as.factor(data$content)
+
+
+# exploring a couple of models (linear)
+## library for linear mixed models
+library(lme4)
+library(lmertest)
+
+glmm1 <- lmer(projective ~ op * verb + (op | workerid) + 
+                 (op | content), data=data)
+
+print(summary(glmm1), cor=F, dig=3)
+
+glmm2 <- lmer(projective ~ op * verb + (verb | workerid) + 
+                (verb | content), data=data)
+
+print(summary(glmm2), cor=F, dig=3)
+
+
+
---
+++ results/main/meta-analysis/projectivity/rscripts/graphs.R
@@ -0,0 +1,83 @@
+
+### summary statistics and graphs for projection data ------
+
+# set wd to script dir
+this.dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(this.dir)
+
+# load data
+data <- read.csv("../data/data_combined.csv", header = TRUE, sep = ",")
+
+# load helper functions
+source('../../helpers.R')
+
+# libraries for manipulating dataframes, and plotting
+library(tidyverse)
+library(ggplot2)
+
+# projectivity by operator -----
+proj_means_op = data %>% group_by(op) %>%
+  summarize(Mean = mean(projective), CILow = ci.low(projective), 
+            CIHigh = ci.high(projective)) %>%
+  mutate(YMin = Mean - CILow, YMax = Mean + CIHigh, 
+         op = fct_reorder(as.factor(op),Mean)) %>% ungroup()
+
+proj_means_op %>% mutate(op = fct_reorder(op, Mean, 
+                                          .fun = mean)) %>% 
+  ggplot(aes(x = op, y=Mean, color = op)) +
+  # coord_cartesian(ylim = c(0,1)) +
+  geom_point(aes(shape = op), size = 4) + 
+  scale_shape_manual(values = c("M", "N", "Q", "C")) +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax), width=0.1) +
+  geom_line() + 
+  labs(title = "Mean projectivity by operator")+
+  theme_bw() +
+  scale_color_brewer(palette = "PRGn")
+
+ggsave("../graphs/proj-by-op.pdf",height=4,width=5)
+
+
+
+# projectivity by verb -----
+proj_means_v = data %>% group_by(verb) %>%
+  summarize(Mean = mean(projective), CILow = ci.low(projective), 
+            CIHigh = ci.high(projective)) %>%
+  mutate(YMin = Mean - CILow, YMax = Mean + CIHigh, 
+         verb = fct_reorder(as.factor(verb),Mean)) %>% ungroup()
+
+proj_means_v %>% mutate(verb = fct_reorder(verb, Mean, 
+                                           .fun = mean)) %>% 
+  ggplot(aes(x=verb, y=Mean)) +
+  coord_cartesian(ylim=c(0,1)) +
+  geom_point(color = "blue") +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax), width=0.1, color = "blue") +
+  geom_line() + 
+  labs(title = "Mean projectivity by predicate")+
+  theme_bw()
+
+ggsave("../graphs/proj-by-verb.pdf",height=6.5,width=14)
+
+
+# projectivity by verb and operator ----
+proj_means = data %>% group_by(verb, op) %>%
+  summarize(Mean = mean(projective), CILow = ci.low(projective), 
+            CIHigh = ci.high(projective)) %>%
+  mutate(YMin = Mean - CILow, YMax = Mean + CIHigh, 
+         verb = fct_reorder(as.factor(verb),Mean)) %>% ungroup()
+
+proj_means %>% mutate(verb = fct_reorder(verb, Mean, 
+                                         .fun = mean)) %>% 
+  mutate(op = fct_reorder(op, Mean, .fun = mean)) %>% 
+  ggplot(aes(x=verb, y=Mean, group = op, color = op)) +
+  coord_cartesian(ylim=c(0,1)) +
+  geom_point(aes(shape = op), size = 4) + 
+  scale_shape_manual(values = c("M", "N", "Q", "C")) +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=0.1) +
+  geom_line() + 
+  labs(title = "Mean projectivity by predicate and operator")+
+  theme_bw() +
+  scale_color_brewer(palette = "PRGn")
+
+ggsave("../graphs/proj-by-both.pdf",height=6.5,width=14)
+
+
