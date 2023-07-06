a7cd10a95c825bf122e40f3e0e3cfe38d9dae702
judith-tonhauser <tonhauser.1@osu.edu>
2023-07-06 09:28:47 +0200

    ran pilot1 of 13_explIg, detected problem with design

--- experiments/pilots/13_explicitIgnorance/pilot1/server.py
+++ experiments/pilots/13_explicitIgnorance/pilot1/server.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 
-# Demo of bottle + gevent
+# Demo of bottle + gevent.
 # Based on https://bottlepy.org/docs/dev/async.html
 
-import os
-import uuid
+import os, sys, uuid
 
 from gevent import monkey; monkey.patch_all()
 from bottle import route, request, run, static_file, HTTPError
 
-# These serve the experiment:
+# The following functions serve the parts of the experiment:
+
+# The @route part specified the path in the URL used in the browser.
+# The subsequent functions locates and loads the data from the hard
+# drive.
 
 @route('/')
 def experiment():
     return static_file("experiment.html", root='')   
 
-@route('/helpers.js')
+@route('helpers.js')
 def send_helpers():
     return static_file("helpers.js", root='') 
 
@@ -37,11 +40,12 @@ def jspsych(filename):
 
 @route('/shared/<filename:re:.+>')
 def send_shared(filename):
-    return static_file(filename, root='shared/')           
+    return static_file(filename, root='shared/') 
 
-# This stores the collected data in data/ with a random file name:
+# The function below stores the collected data in directory `data`
+# with a random filename following the pattern `results_XYZ.csv`.
 
-# Accept no more than max_data in POST requests:
+# Accept no more than max_data bytes in POST requests:
 max_data = 1024 * 5000
 
 @route('/store', method="POST")
@@ -56,8 +60,17 @@ def store():
             raise RuntimeError("A file named 'data' exists where a directory with that name is expected.")
     else:
         os.mkdir("data")
-    filename = str(uuid.uuid4()) + ".csv"
-    with open("data/" + "results_" + filename, "x") as f:
+    filename = f"data/results_{uuid.uuid4()}.csv"
+    with open(filename, "x") as f:
         f.write(filedata)
 
-run(host='0.0.0.0', port=80, server='gevent')
+try:
+    run(host='0.0.0.0', port=80, server='gevent')
+except OSError as err:
+    if err.args[0] == 98:
+        print("ERROR: The IP address is already in use.  End other process and try again.  Quitting.")
+    else:
+        print(err)
+    os.remove("nohup.pid")
+    sys.exit(-1)
+
