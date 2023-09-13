bd56d70faae95db7dc59ec4033a68496f847e0a6
judith-tonhauser <tonhauser.1@osu.edu>
2023-09-13 12:50:47 +0200

    created talk for Nicoles conference

--- results/main/13explicitIgnorance/rscripts/analysis.R
+++ results/main/13explicitIgnorance/rscripts/analysis.R
@@ -6,11 +6,9 @@ this.dir <- dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(this.dir)
 
 # load required packages
-require(tidyverse)
+library(tidyverse)
 library(lme4)
-
-# load helper functions
-source('../../../helpers.R')
+library(emmeans)
 
 # load cleaned data
 d = read_tsv("../data/cd.tsv")
@@ -20,28 +18,37 @@ table(d$context)
 
 length(unique(d$participantID)) #370 participants
 
-# predict acceptability rating in comparison to least acceptable (continue) ----
+# predict naturalness rating from expression in explicit ignorance context ----
 
-# target data: explicit ignorance context
+# target data: explicit ignorance context, no controls
 t = d %>%
-  filter(context == "explicitIgnorance")
+  filter(context == "explicitIgnorance") %>%
+  filter(expression != "controlGood1" & expression != "controlGood2")
 
-# merge the two controls into one
-table(t$expression)
+# sort the expressions by mean naturalness rating (for ease of reading multiple comparisons)
+means = t %>%
+  group_by(expression) %>%
+  summarize(mean = mean(response)) %>%
+  mutate(expression = fct_reorder(as.factor(expression),mean)) 
+means
+
 t = t %>%
-  mutate(expression = recode(expression, "controlGood1" = "controls", "controlGood2" = "controls"))
+  mutate(expression = fct_relevel(expression,levels(means$expression)))
+levels(t$expression)
 
-# set reference level
-# create item as combination of predicate and complement clause
-# cd$item = as.factor(paste(cd$verb,cd$content))
+# set reference level (continue as the least natural)
+t = t %>% mutate(expression = fct_relevel(expression, "continue"))
 
 # model
-str(t$expression)
-t$expression <- as.factor(t$expression)
-t$expression <- relevel(t$expression, ref = "continue")
-names(t)
-m = lmer(response ~ expression + (1|participantID), data = t, REML=F)
+m = lmer(response ~ expression + (1|participantID) + (1|cc), data = t, REML=F)
 summary(m)
+
+# pairwise comparisons on expression using tukey
+pc = lsmeans(m, revpairwise~expression, adjust="tukey",lmerTest.limit = 6660,pbkrtest.limit = 6660)
+options(max.print=2000)
+pc
+
+################
 
 # predict acceptability rating from predicate and context ----
 
